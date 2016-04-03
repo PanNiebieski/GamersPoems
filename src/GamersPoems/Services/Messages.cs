19bb67e8f644d9db1670363849cda04f0cf9fe52
Cezary Walenciuk <Cezary Walenciuk>
2016-04-03 13:39:01 +0200

    First Curse End

---
+++ src/GamersPoems/Services/Messages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamersPoems.Services
+{
+    public interface IMessages
+    {
+        string Hello();
+    }
+
+    public class Messages : IMessages
+    {
+        public string Hello()
+        {
+            return "Cez Hello";
+        }
+    }
+}
